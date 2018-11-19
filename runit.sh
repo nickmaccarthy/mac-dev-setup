6b0ee4cff61d55ac7aa0e23ca03bea5e7ecaa4be
Nick MacCarthy <nickmaccarthy@gmail.com>
2018-11-19 10:37:00 -0800

    fixes for ansible 2.7 and added custom.config.yml as an example

--- runit.sh
+++ runit.sh
@@ -2,8 +2,8 @@
 # We need to get pip installed first
 sudo /usr/bin/easy_install pip
 # Now we can install Ansible
-sudo pip install 'ansible>=2.3.0'
+pip install 'ansible>=2.3.0'
 # Install our ansible galaxy roles
-sudo ansible-galaxy install -r requirements.yml
+ansible-galaxy install -r requirements.yml
 # Now we can run ansible
 ansible-playbook playbook.yml -vv --ask-sudo-pass
