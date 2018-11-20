3fd6672ae16074ae9e078f683d5dfb3777b4c087
Nick MacCarthy <nickmaccarthy@gmail.com>
2018-11-20 09:03:12 -0500

    bug fixes and custom.config.yml added

--- runit.sh
+++ runit.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 # We need to get pip installed first
-sudo /usr/bin/easy_install pip
+if ! [ -x "$(command -v pip)" ]; then
+  echo 'Pip is not installed, installing python pip' >&2
+  sudo /usr/bin/easy_install pip
+else
+  echo "PIP is already installed... moving on..."
+fi
+
 # Now we can install Ansible
-pip install 'ansible>=2.3.0'
+if ! [ -x "$(command -v ansible-playbook)" ]; then
+  echo 'Ansible is not installed, installing Ansible' >&2
+  pip install 'ansible>=2.3.0'
+else
+  echo "Ansible is already installed... moving on..."
+fi
 # Install our ansible galaxy roles
+echo "Installing role requirements for Ansible"
 ansible-galaxy install -r requirements.yml
 # Now we can run ansible
+echo "Running Ansible playbook..."
 ansible-playbook playbook.yml -vv --ask-sudo-pass
