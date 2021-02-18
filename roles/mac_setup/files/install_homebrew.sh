293c25fff9f48342d76555fcea358d17fff9a765
Nick MacCarthy <nickmaccarthy@gmail.com>
2021-02-18 16:48:42 -0500

    added shell checking/setting

--- roles/mac_setup/files/install_homebrew.sh
+++ roles/mac_setup/files/install_homebrew.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
